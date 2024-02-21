443b5ac2f1dbb9e8ddfd77d80a38d32540a6fdd3
maruf-ahmed <marufahamed1110@gmail.com>
2024-02-21 20:11:44 +0600

    added typing animation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function scrollToSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
+  var section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
   }
+}
+
+let typed = new Typed(".auto-type", {
+  strings: ["Web Developer", "UI / UX Designer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
